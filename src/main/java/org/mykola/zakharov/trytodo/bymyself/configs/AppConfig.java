9abdbf42a76b8e8878af9a6de52d1f78ce509c91
zaharovmykola <zaharovmykola@gmail.com>
2020-06-04 07:23:52 +0200

    homework21-spring-java-coding-annotations-changes-teacher-advice

--- src/main/java/org/mykola/zakharov/trytodo/bymyself/configs/AppConfig.java
+++ src/main/java/org/mykola/zakharov/trytodo/bymyself/configs/AppConfig.java
@@ -7,27 +7,52 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    @Bean(name = "warrior")
+    public Warrior warrior () {
+        return new Warrior();
+    }
+
+    @Bean(name = "peasant")
+    public Peasant peasant () {
+        return new Peasant();
+    }
+
+    @Bean(name = "sword")
+    public Sword sword () {
+        return new Sword();
+    }
+
+    @Bean(name = "axe")
+    public Axe axe () {
+        return new Axe();
+    }
+
+    @Bean(name = "plow")
+    public Plow plow () {
+        return new Plow();
+    }
+
     @Bean(name = "warriorUnitWithSword")
-    public Unit warriorUnitWithSword () {
-        Unit u = new Unit(new Warrior(), new Sword(), "Warrior_With_Sword");
+    public Unit warriorUnitWithSword (Warrior warrior, Sword sword) {
+        Unit u = new Unit(warrior, sword, "Warrior_With_Sword");
         return u;
     }
 
     @Bean(name = "warriorUnitWithAxe")
-    public Unit warriorUnitWithAxe () {
-        Unit u = new Unit(new Warrior(), new Axe(), "Warrior_With_Axe");
+    public Unit warriorUnitWithAxe (Warrior warrior, Axe axe) {
+        Unit u = new Unit(warrior, axe, "Warrior_With_Axe");
         return u;
     }
 
     @Bean(name = "peasantUnitWithPlow")
-    public Unit peasantUnitWithPlow () {
-        Unit u = new Unit(new Peasant(), new Plow(), "Peasant_With_Plow");
+    public Unit peasantUnitWithPlow (Peasant peasant, Plow plow) {
+        Unit u = new Unit(peasant, plow, "Peasant_With_Plow");
         return u;
     }
 
     @Bean(name = "peasantUnitWithAxe")
-    public Unit peasantUnitWithAxe () {
-        Unit u = new Unit(new Peasant(), new Axe(), "Peasant_With_Axe");
+    public Unit peasantUnitWithAxe (Peasant peasant, Axe axe) {
+        Unit u = new Unit(peasant, axe, "Peasant_With_Axe");
         return u;
     }
 
